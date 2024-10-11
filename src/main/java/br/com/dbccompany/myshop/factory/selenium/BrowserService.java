a4a6604630a224682d7323f52458eee83f0097e3
Thales Brasileiro <thalesxbrasileiro@gmail.com>
2024-10-10 23:07:31 -0300

    feat: Adicionado o selenium grid

--- src/main/java/br/com/dbccompany/myshop/factory/selenium/BrowserService.java
+++ src/main/java/br/com/dbccompany/myshop/factory/selenium/BrowserService.java
@@ -1,9 +1,12 @@
 package br.com.dbccompany.myshop.factory.selenium;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 public class BrowserService {
@@ -13,10 +16,18 @@ public class BrowserService {
 
     public void initBrowser(String url) {
 
-        driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(60));
-        driver.get(url);
-        driver.manage().window().maximize();
+		try {
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			capabilities.setBrowserName("chrome");
+
+			driver = new RemoteWebDriver(new URL("http://localhost:4444"), capabilities);
+			wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+			driver.get(url);
+			driver.manage().window().maximize();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+
 	}
 
     public void quit() {
